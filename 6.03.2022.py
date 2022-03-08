eea067f06b42898f96c28d027c98df6f755c4d32
Yemen Nur SARI <96724883+yemennursari@users.noreply.github.com>
2022-03-08 23:28:50 +0300

    Initial commit

---
+++ 6.03.2022.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+l=[[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+l.sort
+l
+
+
+# In[21]:
+
+
+list = [[1,2],[3,4],[5,6,7]]
+list.reverse()
+
+for l in list:
+    l.reverse() 
+
+print(list) 
+
